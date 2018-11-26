255c20a5e6bb4c5eb2db8dbe59a9c7cfe5fb73c1
admin <admin@DESKTOP-32NI50E>
2018-11-27 02:26:25 +1000

    sdfgsd

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        phpinfo();// master
+        phpinfo();// masterfgsdf sdf
         echo  'fff';  
         ?>
     </body>
